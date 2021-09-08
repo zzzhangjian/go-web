96ade02d70d2f8cba2dc4f28b122da9b7a381a8b
so0os <so0os@live.cn>
2021-09-09 00:51:03 +0800

    add base engine

---
+++ gw/gw.go
@@ -0,0 +1,42 @@
+package gw
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type HandlerFunc func(w http.ResponseWriter, r *http.Request)
+
+type Engine struct {
+	router map[string]HandlerFunc
+}
+
+func New() *Engine {
+	return &Engine{router: make(map[string]HandlerFunc)}
+}
+
+func (engine *Engine) addRoute(method string, pattern string, hander HandlerFunc) {
+	key := method + "-" + pattern
+	engine.router[key] = hander
+}
+
+func (engine *Engine) Get(pattern string, handler HandlerFunc) {
+	engine.addRoute("GET", pattern, handler)
+}
+
+func (engine *Engine) Post(pattern string, handler HandlerFunc) {
+	engine.addRoute("POST", pattern, handler)
+}
+
+func (engine *Engine) Run(addr string) (err error) {
+	return http.ListenAndServe(addr, engine)
+}
+
+func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	key := req.Method + "-" + req.URL.Path
+	if handler, ok := engine.router[key]; ok {
+		handler(w, req)
+	} else {
+		fmt.Fprintf(w, "404 NOT FOUND: %s\n", req.URL)
+	}
+}
